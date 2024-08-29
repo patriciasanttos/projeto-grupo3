cd6b4a063ba88f782cdd20fa64315791f2f9b468
Patricia Santos <patricia.desenvolveti@gmail.com>
2024-08-29 16:01:34 -0300

    section added

--- app/src/pages/volunteers/index.jsx
+++ app/src/pages/volunteers/index.jsx
@@ -3,16 +3,32 @@ import React from 'react';
 import NavBar from '../../components/navbar/NavBar';
 import Footer from '../../components/footer/Footer';
 
+import "./volunteers.css";
+
 // import { Container } from './styles';
 
 function Volunteers() {
   return (
-    <div className='page_container'>
+    <div className="page_container">
       <NavBar />
 
-      <div>
-        <h1>Voluntários</h1>
-      </div>
+      <section>
+        <h3 className="call-title">FAÇA A DIFERENÇA NA VIDA DE UM ANIMAL</h3>
+        <h1 className="call-subtitle">SEJA UM VOLUNTÁRIO</h1>
+        <p className="call-text call-text-1">
+          Voluntariar-se é uma forma incrível de conectar-se com seres que
+          possuem um amor puro <br />e incondicional para oferecer. Além disso,
+          você estará cercado por uma comunidade de <br />
+          pessoas que compartilham o mesmo amor e respeito pelos animais. <br />
+        </p>
+        <p className='call-text call-text-2'>
+          Não é necessário ter experiência anterior, tudo o que pedimos é um
+          coração aberto e
+          <br />
+          vontade de fazer a diferença. Os animais precisam de você, e cada
+          gesto de amor conta!
+        </p>
+      </section>
 
       <Footer />
     </div>
