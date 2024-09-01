c784de31ff9438077af28cf6bc00df84fbbea8c0
Patricia Santos <patricia.desenvolveti@gmail.com>
2024-09-01 14:21:52 -0300

    section hero added and updated global style

--- app/src/pages/landing_page/index.jsx
+++ app/src/pages/landing_page/index.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React from "react";
 
-import NavBar from '../../components/navbar/NavBar';
-import Footer from '../../components/footer/Footer';
+import NavBar from "../../components/navbar/NavBar";
+import Footer from "../../components/footer/Footer";
 
 // import styles from './styles.css';
+import "./landingPage.scss";
+
+// import images
+import heroDog from "../../assets/images/hero-dog.png";
+import Waves from '../../assets/images/hero-waves.svg'
 
 function LandingPage() {
   return (
-    <div className='page_container'>
+    <div className="page_container">
       <NavBar />
 
-      <div>
-        <h1>Página inicial</h1>
-      </div>
+      <section className="hero">
+        <div className="title-img">
+          <div>
+            <h1 className="hero-title hero-all-text">
+              Há 36 anos cuidando de animais abandonados em Juiz de Fora/MG.
+            </h1>
+            <p className="hero-text hero-all-text">
+              Faça parte dessa missão de amor e esperança!
+            </p>
+            <button className="btn-donation">Doe Agora</button>
+          </div>
+          <div>
+            <img className="hero-dog" src={heroDog} alt="" />
+          </div>
+        </div>
+        <img className="waves" src={Waves} alt="" />
+      </section>
 
       <Footer />
     </div>
