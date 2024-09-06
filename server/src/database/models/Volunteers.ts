952f0037b94d50537e584cf632ec772f71876b17
unknown <hector.02220x@gmail.com>
2024-09-05 22:36:44 -0300

    Feat: Volunteer model created

---
+++ server/src/database/models/Volunteers.ts
@@ -0,0 +1,63 @@
+import { DataTypes, Model, Optional } from "sequelize";
+import sequelize from "..";
+
+interface VolunteerInfo {
+    id: number,
+    name: string,
+    email: string,
+    phone: number,
+    address: string,
+    availability: string,
+    created_at?: string,
+    updated_at?: string,
+}
+
+type VolunteerInfoCreation = Optional<VolunteerInfo, 'id'>;
+
+class Volunteer extends Model<VolunteerInfo, VolunteerInfoCreation> {}
+
+Volunteer.init({
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    address: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    availability: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false,
+    },
+  }, {
+    tableName: "volunteers",
+    sequelize,
+  }
+);
+
+export default Volunteer;
