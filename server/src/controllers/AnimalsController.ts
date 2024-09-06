a33ebb8417c5e6e0a723a8fc528df56f360375c1
unknown <hector.02220x@gmail.com>
2024-09-06 14:45:17 -0300

    Feat: Sponsorships controller developed

--- server/src/controllers/AnimalsController.ts
+++ server/src/controllers/AnimalsController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+
 import animalsRepository from '../repositories/animals.repository';
 
 class AnimalController {
--- server/src/controllers/SponsorshipsController.ts
+++ server/src/controllers/SponsorshipsController.ts
@@ -1,26 +1,47 @@
 import { Request, Response } from 'express';
 
-import { SponsorshipType } from "../utils/types";
+import sponsorshipsRepository from '../repositories/sponsorships.repository';
 
 class SponsorshipsController {
     async getAll(req: Request, res: Response) {
+        const response = await sponsorshipsRepository.getAllSponsorships();
 
+        res.status(response.code).json(response.data);
     }
 
     async getById(req: Request, res: Response) {
+        const { id } = req.params;
 
+        const response = await sponsorshipsRepository.getSponsorshipById(Number(id));
+
+        res.status(response.code).json(response.data);
     }
 
     async create(req: Request, res: Response) {
+        const data = req.body;
 
+        if (data.name === undefined || data.email === undefined || data.phone === undefined || data.animal_id === undefined)
+            return res.status(400).json({ error: 'Invalid body request' });
+
+        const response = await sponsorshipsRepository.createSponsorship(data);
+
+        return res.status(response.code).json(response.data);
     }
 
     async update(req: Request, res: Response) {
+        const data = req.body;
 
+        const response = await sponsorshipsRepository.updateSponsorship(data);
+
+        return res.status(response.code).json(response.data);
     }
 
     async delete(req: Request, res: Response) {
+        const id = req.params;
 
+        const response = await sponsorshipsRepository.deleteSponsorship(Number(id));
+
+        return res.status(response.code).json(response.data);
     }
 }
 
