561c1d10ccc4909fee8ff919a720572d4e96fb63
unknown <hector.02220x@gmail.com>
2024-09-05 22:29:53 -0300

    Feat: Animal model created

---
+++ server/src/database/models/Animal.ts
@@ -0,0 +1,108 @@
+import { DataTypes, Model, Optional } from "sequelize";
+import sequelize from "..";
+
+interface AnimalInfo {
+    id: string,
+    name: string,
+    species: string,
+    race?: string
+    size: string,
+    color?: string,
+    vacine?: number,
+    castrated?: string,
+    age?: number,
+    gender?: string,
+    temperament?: string,
+    bay?: number,
+    sector?: string,
+    status: string,
+    observation?: string,
+    created_at?: string,
+    updated_at?: string,
+}
+
+type AnimalInfoCreation = Optional<AnimalInfo, 'id'>;
+
+class Animal extends Model<AnimalInfo, AnimalInfoCreation> {}
+
+Animal.init({
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    species: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    race: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    size: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    color: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    vacine: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    castrated: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    gender: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    temperament: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    bay: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    sector: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    observation: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false,
+    },
+  }, {
+    tableName: "Animal",
+    sequelize,
+  }
+);
+
+export default Animal;
