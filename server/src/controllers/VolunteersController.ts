0059af18bc7d77d8d05c84e19f4a43f4f041a46d
unknown <hector.02220x@gmail.com>
2024-09-06 14:52:15 -0300

    Feat: Volunteers controller developed

--- server/src/controllers/VolunteersController.ts
+++ server/src/controllers/VolunteersController.ts
@@ -1,26 +1,47 @@
 import { Request, Response } from 'express';
 
-import { VolunteerType } from "../utils/types";
+import volunteerRepository from '../repositories/volunteer.repository';
 
 class VolunteersController {
     async getAll(req: Request, res: Response) {
+        const response = await volunteerRepository.getAllVolunteers();
 
+        res.status(response.code).json(response.data);
     }
 
     async getById(req: Request, res: Response) {
+        const { id } = req.params;
 
+        const response = await volunteerRepository.getVolunteerById(Number(id));
+
+        res.status(response.code).json(response.data);
     }
 
     async create(req: Request, res: Response) {
+        const data = req.body;
 
+        if (data.name === undefined || data.email === undefined || data.phone === undefined || data.availability === undefined)
+            return res.status(400).json({ error: 'Invalid body request' });
+
+        const response = await volunteerRepository.createVolunteer(data);
+
+        return res.status(response.code).json(response.data);
     }
 
     async update(req: Request, res: Response) {
+        const data = req.body;
 
+        const response = await volunteerRepository.updateVolunteer(data);
+
+        return res.status(response.code).json(response.data);
     }
 
     async delete(req: Request, res: Response) {
+        const { id } = req.params;
 
+        const response = await volunteerRepository.deleteVolunteer(Number(id));
+
+        return res.status(response.code).json(response.data);
     }
 }
 
