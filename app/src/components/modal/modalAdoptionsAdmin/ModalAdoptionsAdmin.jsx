2ab6342bf0db2c45d8c1448901201ba143aaf900
Patricia Santos <patriciasanttos.dev@gmail.com>
2024-09-13 20:12:57 -0300

    added adoption list and tutors modal

---
+++ app/src/components/modal/modalAdoptionsAdmin/ModalAdoptionsAdmin.jsx
@@ -0,0 +1,218 @@
+import { useEffect, useState } from "react";
+import Modal from "../index";
+import { IMaskInput } from "react-imask";
+
+const ModalAdoptionsAdmin = ({
+  isOpen,
+  onModalClose,
+  selectedTutor,
+  updateTutorsList,
+  createTutorsList,
+}) => {
+  const initialFormTutors = {
+    name: "",
+    email: "",
+    phoneNumber: "",
+    address: "",
+    nameAnimal: "",
+    sex: "",
+    temperament: "",
+    castrated: "",
+    healthHistory: "",
+    adoptionDate: "",
+    tutorsInfo: "",
+  };
+  const [formTutors, setFormTutors] = useState(initialFormTutors);
+
+  useEffect(() => {
+    setFormTutors({
+      ...selectedTutor,
+    });
+  }, [selectedTutor, isOpen]);
+
+  const onClickSave = () => {
+    if (selectedTutor) {
+      updateTutorsList(formTutors);
+    } else {
+      createTutorsList(formTutors);
+    }
+  };
+
+  const onClickModalClose = () => {
+    onModalClose();
+    setFormTutors(initialFormTutors);
+  };
+
+  const getFormState = (field) => {
+    return formTutors && formTutors[field] ? formTutors[field] : "";
+  };
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onModalClose={onClickModalClose}
+      title={
+        selectedTutor ? "Edite os dados do tutor" : "Adicione um novo tutor"
+      }
+    >
+      <form action="" className="modal-tutors-form modal-form">
+        <div className="al-modal-form">
+          <input
+            type="text"
+            name="name"
+            placeholder="Nome do Tutor"
+            value={getFormState("name")}
+            onChange={(e) =>
+              setFormTutors({ ...formTutors, name: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="E-mail"
+            value={getFormState("email")}
+            onChange={(e) =>
+              setFormTutors({ ...formTutors, email: e.target.value })
+            }
+          />
+        </div>
+        <div className="al-modal-form">
+          <IMaskInput
+            type="text"
+            name="Celular"
+            placeholder="Contato"
+            value={getFormState("phoneNumber")}
+            onAccept={(value, maskRef, e) =>
+              setFormTutors({ ...formTutors, phoneNumber: value })
+            }
+            mask={"(00) 00000-0000"}
+          />
+
+          <input
+            type="text"
+            name="address"
+            id=""
+            placeholder="Endereço completo"
+            value={getFormState("address")}
+            onChange={(e) =>
+              setFormTutors({ ...formTutors, address: e.target.value })
+            }
+          />
+        </div>
+
+        <div className="al-modal-form">
+          <input
+            type="text"
+            name="nameAnimal"
+            id=""
+            placeholder="Nome do animal"
+            value={getFormState("nameAnimal")}
+            onChange={(e) =>
+              setFormTutors({
+                ...formTutors,
+                nameAnimal: e.target.value,
+              })
+            }
+          />
+
+          <input
+            type="text"
+            name="sex"
+            id=""
+            placeholder="Sexo do animal"
+            value={getFormState("sex")}
+            onChange={(e) =>
+              setFormTutors({
+                ...formTutors,
+                sex: e.target.value,
+              })
+            }
+          />
+        </div>
+
+        <div className="al-modal-form">
+          <input
+            type="text"
+            name="temperament"
+            id=""
+            placeholder="Temperamento do animal"
+            value={getFormState("temperament")}
+            onChange={(e) =>
+              setFormTutors({
+                ...formTutors,
+                temperament: e.target.value,
+              })
+            }
+          />
+
+          <input
+            type="text"
+            name="castrated"
+            id=""
+            placeholder="Castrado"
+            value={getFormState("castrated")}
+            onChange={(e) =>
+              setFormTutors({
+                ...formTutors,
+                castrated: e.target.value,
+              })
+            }
+          />
+        </div>
+        <div className="al-modal-form">
+          <input
+            type="text"
+            name="healthHistory"
+            id=""
+            placeholder="Histórico de saúde"
+            value={getFormState("healthHistory")}
+            onChange={(e) =>
+              setFormTutors({
+                ...formTutors,
+                healthHistory: e.target.value,
+              })
+            }
+          />
+
+          <input
+            type="text"
+            name="adoptionDate"
+            id=""
+            placeholder="Data da adoção"
+            value={getFormState("adoptionDate")}
+            onChange={(e) =>
+              setFormTutors({
+                ...formTutors,
+                adoptionDate: e.target.value,
+              })
+            }
+          />
+        </div>
+        <textarea
+          rows="8"
+          cols="10"
+          name="tutorsInfo"
+          id=""
+          placeholder="Adicione informações importantes sobre o tutor ou animal"
+          value={getFormState("tutorsInfo")}
+          onChange={(e) =>
+            setFormTutors({
+              ...formTutors,
+              tutorsInfo: e.target.value,
+            })
+          }
+        />
+      </form>
+      <div className="align-btn-modal">
+        <button onClick={onClickSave} className="btn-modal">
+          {selectedTutor ? "Editar" : "Adicionar"}
+        </button>
+        <button onClick={onModalClose} className="btn-modal grey-btn">
+          Cancelar
+        </button>
+      </div>
+    </Modal>
+  );
+};
+
+export default ModalAdoptionsAdmin;
--- app/src/pages/private/adoptions/index.jsx
+++ app/src/pages/private/adoptions/index.jsx
@@ -1,14 +1,134 @@
-import React from "react";
+import React, { useState } from "react";
 
 import AdminNavBar from "../../../components/admin_navbar/AdminNavBar";
+import ModalAdoptionsAdmin from "../../../components/modal/modalAdoptionsAdmin/ModalAdoptionsAdmin";
+import AdminList from "../../../components/admin_list/AdminList";
 
 import "./styles.scss";
 
 function Adoptions() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedTutor, setSelectedTutor] = useState(null);
+
+  // To do: Trazer lista do back-end
+  const [tutorsList, setTutorsList] = useState([
+    {
+      id: 1,
+      name: "Tutor 1",
+      email: "turo1@gmail.com",
+      phoneNumber: "(11) 11111-1111",
+      address: "Rua tutor 1",
+      nameAnimal: "Hector",
+      sex: "Macho",
+      temperament: "Ansioso",
+      castrated: "Sim",
+      healthHistory: "Dúvida",
+      adoptionDate: "13/09/2024",
+      tutorsInfo: "Nada a declarar",
+    },
+    {
+      id: 2,
+      name: "Tutor 2",
+      email: "turo2@gmail.com",
+      phoneNumber: "(22) 22222-2222",
+      address: "Rua tutor 2",
+      nameAnimal: "Júlia",
+      sex: "Fêmea",
+      temperament: "Ansiosa",
+      castrated: "Sim",
+      healthHistory: "Dúvida",
+      adoptionDate: "13/09/2024",
+      tutorsInfo: "Nada a declarar",
+    },
+    {
+      id: 3,
+      name: "Tutor 3",
+      email: "turo3@gmail.com",
+      phoneNumber: "(33) 33333-3333",
+      address: "Rua tutor 3",
+      nameAnimal: "Andressa",
+      sex: "Fêmea",
+      temperament: "Ansiosa",
+      castrated: "Sim",
+      healthHistory: "Dúvida",
+      adoptionDate: "13/09/2024",
+      tutorsInfo: "Nada a declarar",
+    },
+  ]);
+
+  const columns = [
+    {
+      title: "Nome",
+      rowKey: "name",
+    },
+    {
+      title: "E-mail",
+      rowKey: "email",
+    },
+    {
+      title: "Celular",
+      rowKey: "phoneNumber",
+    },
+    {
+      title: "Endereço",
+      rowKey: "address",
+    },
+    {
+      title: "Adotou",
+      rowKey: "nameAnimal",
+    },
+  ];
+
+  // To do: Enviar para o back-end
+  const updateTutorsList = (tutor) => {
+    let tutors = [...tutorsList];
+    tutors[tutor.id - 1] = {
+      ...tutor,
+    };
+    setTutorsList(tutors);
+    setIsModalOpen(false);
+  };
+
+  // To do: Enviar para o back-end
+  const createTutorsList = (tutor) => {
+    let tutors = [...tutorsList];
+    tutors.push({
+      ...tutor,
+      id: tutorsList.length + 1,
+    });
+    setTutorsList(tutors);
+    setIsModalOpen(false);
+  };
+
+  const onClickEditTutor = (tutor) => {
+    setIsModalOpen(true);
+    setSelectedTutor(tutor);
+  };
+
+  const onClickNewTutor = () => {
+    setIsModalOpen(true);
+    setSelectedTutor(null);
+  };
+
   return (
-    <AdminNavBar headerTitle="Adoções">
-      <h1>Adicione o conteúdo aqui</h1>
-    </AdminNavBar>
+    <>
+      <AdminNavBar headerTitle="Adoções">
+        {/* substituir o button pelo ícone de adicionar*/}
+        <button onClick={onClickNewTutor}>Adicionar</button>
+        <AdminList
+          columns={columns}
+          rows={tutorsList}
+          onClickRow={onClickEditTutor}
+        />
+      </AdminNavBar>
+      <ModalAdoptionsAdmin
+        isOpen={isModalOpen}
+        onModalClose={() => setIsModalOpen(false)}
+        selectedTutor={selectedTutor}
+        updateTutorsList={updateTutorsList}
+        createTutorsList={createTutorsList}
+      />
+    </>
   );
 }
 
