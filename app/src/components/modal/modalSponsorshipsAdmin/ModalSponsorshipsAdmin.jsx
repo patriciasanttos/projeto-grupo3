e670627dfbdec4af2897412f15a46491c8e3c57c
Patricia Santos <patriciasanttos.dev@gmail.com>
2024-09-14 11:17:14 -0300

    Added sponsorships modal admin

---
+++ app/src/components/modal/modalSponsorshipsAdmin/ModalSponsorshipsAdmin.jsx
@@ -0,0 +1,220 @@
+import { useEffect, useState } from "react";
+import Modal from "../index";
+import { IMaskInput } from "react-imask";
+
+const ModalSponsorshipsAdmin = ({
+  isOpen,
+  onModalClose,
+  selectedSponsor,
+  updateSponsorsList,
+  createSponsorsList,
+}) => {
+  const initialFormSponsors = {
+    name: "",
+    email: "",
+    phoneNumber: "",
+    sponsorDate: "",
+    nameAnimal: "",
+    sex: "",
+    temperament: "",
+    castrated: "",
+    healthHistory: "",
+    adoptionStatus: "",
+    sponsorsInfo: "",
+  };
+  const [formSponsors, setFormSponsors] = useState(initialFormSponsors);
+
+  useEffect(() => {
+    setFormSponsors({
+      ...selectedSponsor,
+    });
+  }, [selectedSponsor, isOpen]);
+
+  const onClickSave = () => {
+    if (selectedSponsor) {
+      updateSponsorsList(formSponsors);
+    } else {
+      createSponsorsList(formSponsors);
+    }
+  };
+
+  const onClickModalClose = () => {
+    onModalClose();
+    setFormSponsors(initialFormSponsors);
+  };
+
+  const getFormState = (field) => {
+    return formSponsors && formSponsors[field] ? formSponsors[field] : "";
+  };
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onModalClose={onClickModalClose}
+      title={
+        selectedSponsor
+          ? "Edite os dados do padrinho"
+          : "Adicione um novo padrinho"
+      }
+    >
+      <form action="" className="modal-sponsors-form modal-form">
+        <div className="al-modal-form">
+          <input
+            type="text"
+            name="name"
+            placeholder="Nome do Padrinho"
+            value={getFormState("name")}
+            onChange={(e) =>
+              setFormSponsors({ ...formSponsors, name: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="E-mail"
+            value={getFormState("email")}
+            onChange={(e) =>
+              setFormSponsors({ ...formSponsors, email: e.target.value })
+            }
+          />
+        </div>
+        <div className="al-modal-form">
+          <IMaskInput
+            type="text"
+            name="phoneNumber"
+            placeholder="Contato"
+            value={getFormState("phoneNumber")}
+            onAccept={(value, maskRef, e) =>
+              setFormSponsors({ ...formSponsors, phoneNumber: value })
+            }
+            mask={"(00) 00000-0000"}
+          />
+
+          <input
+            type="text"
+            name="sponsorDate"
+            id=""
+            placeholder="Data do apadrinhamento"
+            value={getFormState("sponsorDate")}
+            onChange={(e) =>
+              setFormSponsors({ ...formSponsors, sponsorDate: e.target.value })
+            }
+          />
+        </div>
+
+        <div className="al-modal-form">
+          <input
+            type="text"
+            name="nameAnimal"
+            id=""
+            placeholder="Nome do animal"
+            value={getFormState("nameAnimal")}
+            onChange={(e) =>
+              setFormSponsors({
+                ...formSponsors,
+                nameAnimal: e.target.value,
+              })
+            }
+          />
+
+          <input
+            type="text"
+            name="sex"
+            id=""
+            placeholder="Sexo do animal"
+            value={getFormState("sex")}
+            onChange={(e) =>
+              setFormSponsors({
+                ...formSponsors,
+                sex: e.target.value,
+              })
+            }
+          />
+        </div>
+
+        <div className="al-modal-form">
+          <input
+            type="text"
+            name="temperament"
+            id=""
+            placeholder="Temperamento do animal"
+            value={getFormState("temperament")}
+            onChange={(e) =>
+              setFormSponsors({
+                ...formSponsors,
+                temperament: e.target.value,
+              })
+            }
+          />
+
+          <input
+            type="text"
+            name="castrated"
+            id=""
+            placeholder="Castrado"
+            value={getFormState("castrated")}
+            onChange={(e) =>
+              setFormSponsors({
+                ...formSponsors,
+                castrated: e.target.value,
+              })
+            }
+          />
+        </div>
+        <div className="al-modal-form">
+          <input
+            type="text"
+            name="healthHistory"
+            id=""
+            placeholder="Histórico de saúde"
+            value={getFormState("healthHistory")}
+            onChange={(e) =>
+              setFormSponsors({
+                ...formSponsors,
+                healthHistory: e.target.value,
+              })
+            }
+          />
+
+          <input
+            type="text"
+            name="adoptionStatus"
+            id=""
+            placeholder="Status de adoção"
+            value={getFormState("adoptionStatus")}
+            onChange={(e) =>
+              setFormSponsors({
+                ...formSponsors,
+                adoptionStatus: e.target.value,
+              })
+            }
+          />
+        </div>
+        <textarea
+          rows="8"
+          cols="10"
+          name="sponsorsInfo"
+          id=""
+          placeholder="Adicione informações importantes sobre o padrinho ou animal"
+          value={getFormState("sponsorsInfo")}
+          onChange={(e) =>
+            setFormSponsors({
+              ...formSponsors,
+              sponsorsInfo: e.target.value,
+            })
+          }
+        />
+      </form>
+      <div className="align-btn-modal">
+        <button onClick={onClickSave} className="btn-modal">
+          {selectedSponsor ? "Editar" : "Adicionar"}
+        </button>
+        <button onClick={onModalClose} className="btn-modal grey-btn">
+          Cancelar
+        </button>
+      </div>
+    </Modal>
+  );
+};
+
+export default ModalSponsorshipsAdmin;
--- app/src/pages/private/sponsorships/index.jsx
+++ app/src/pages/private/sponsorships/index.jsx
@@ -1,14 +1,133 @@
-import React from "react";
+import React, { useState } from "react";
 
 import AdminNavBar from "../../../components/admin_navbar/AdminNavBar";
+import ModalSponsorshipsAdmin from "../../../components/modal/modalSponsorshipsAdmin/ModalSponsorshipsAdmin";
+import AdminList from "../../../components/admin_list/AdminList";
 
 import "./styles.scss";
 
 function Sponsorships() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedSponsor, setSelectedSponsor] = useState(null);
+
+  // To do: Trazer lista do back-end
+  const [sponsorsList, setSponsorsList] = useState([
+    {
+      id: 1,
+      name: "Padrinho 1",
+      email: "padrinho1@gmail.com",
+      phoneNumber: "(11) 11111-1111",
+      sponsorDate: "14/09/24",
+      nameAnimal: "Hector",
+      sex: "Macho",
+      temperament: "Ansioso",
+      castrated: "Sim",
+      healthHistory: "Bom",
+      adoptionStatus: "",
+      sponsorsInfo: "",
+    },
+    {
+      id: 2,
+      name: "Padrinho ",
+      email: "padrinho2@gmail.com",
+      phoneNumber: "(22) 22222-2222",
+      sponsorDate: "14/09/24",
+      nameAnimal: "Hector",
+      sex: "Macho",
+      temperament: "Ansioso",
+      castrated: "Sim",
+      healthHistory: "Bom",
+      adoptionStatus: "",
+      sponsorsInfo: "",
+    },
+    {
+      id: 3,
+      name: "Padrinho 3",
+      email: "padrinho3@gmail.com",
+      phoneNumber: "(33) 33333-3333",
+      sponsorDate: "14/09/24",
+      nameAnimal: "Hector",
+      sex: "Macho",
+      temperament: "Ansioso",
+      castrated: "Sim",
+      healthHistory: "Bom",
+      adoptionStatus: "",
+      sponsorsInfo: "",
+    },
+  ]);
+
+  const columns = [
+    {
+      title: "Nome",
+      rowKey: "name",
+    },
+    {
+      title: "E-mail",
+      rowKey: "email",
+    },
+    {
+      title: "Celular",
+      rowKey: "phoneNumber",
+    },
+    {
+      title: "Apadrinhou",
+      rowKey: "nameAnimal",
+    },
+    {
+      title: "Status",
+      rowKey: "adoptionStatus",
+    },
+  ];
+
+  // To do: Enviar para o back-end
+  const updateSponsorsList = (sponsor) => {
+    let sponsors = [...sponsorsList];
+    sponsors[sponsor.id - 1] = {
+      ...sponsor,
+    };
+    setSponsorsList(sponsors);
+    setIsModalOpen(false);
+  };
+
+  // To do: Enviar para o back-end
+  const createSponsorsList = (sponsor) => {
+    let sponsors = [...sponsorsList];
+    sponsors.push({
+      ...sponsor,
+      id: sponsorsList.length + 1,
+    });
+    setSponsorsList(sponsors);
+    setIsModalOpen(false);
+  };
+
+  const onClickEditSponsor = (sponsor) => {
+    setIsModalOpen(true);
+    setSelectedSponsor(sponsor);
+  };
+
+  const onClickNewSponsor = () => {
+    setIsModalOpen(true);
+    setSelectedSponsor(null);
+  };
   return (
-    <AdminNavBar headerTitle="Apadrinhamento">
-      <h1>Adicione o conteúdo aqui</h1>
-    </AdminNavBar>
+    <>
+      <AdminNavBar headerTitle="Apadrinhamento">
+        {/* substituir o button pelo ícone de adicionar*/}
+        <button onClick={onClickNewSponsor}>Adicionar</button>
+        <AdminList
+          columns={columns}
+          rows={sponsorsList}
+          onClickRow={onClickEditSponsor}
+        />
+      </AdminNavBar>
+      <ModalSponsorshipsAdmin
+        isOpen={isModalOpen}
+        onModalClose={() => setIsModalOpen(false)}
+        selectedSponsor={selectedSponsor}
+        updateSponsorsList={updateSponsorsList}
+        createSponsorsList={createSponsorsList}
+      />
+    </>
   );
 }
 
