7981e9abed1bd93d84d46b3b363de213d0b56222
unknown <hector.02220x@gmail.com>
2024-09-15 18:12:31 -0300

    Feat: API implemented to Login Page

--- app/src/pages/private/login/index.jsx
+++ app/src/pages/private/login/index.jsx
@@ -1,18 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './styles.scss';
 
 import Logo from "../../../assets/images/logo.svg"
+import { loginAdmin } from '../../../services/api/admins';
+import { Navigate } from 'react-router-dom';
 
-const Admin = () => {
+function Admin() {
+  const [ userState, setUserState ] = useState({
+    user: '',
+    password: ''
+  });
+  const [ logged, setLogged ] = useState(false);
+
+  useEffect(() => {
+    if (window.localStorage.getItem('login'))
+      return setLogged(true);
+  }, []);
+
+  const login = () => {
+    loginAdmin(userState)
+      .then(res => {
+        window.alert('Logged in: ' + JSON.stringify(res));
+        window.localStorage.setItem('login', JSON.stringify(res))
+        return setLogged(true);
+      })
+      .catch(error => {
+        window.alert(JSON.stringify(error.message));
+      });
+  }
+
+  if (logged)
+    return <Navigate to='/admin/control_panel' />
+
   return (
     <div className='admin'>
         <img src={Logo} alt="ong-logo" />
         <div className='admin-container'>
             <label htmlFor=""> Nome de usuário ou e-mail</label>
-            <input className='admin-entrie' type="text" placeholder='Usuário ou e-mail'/>
+            <input 
+              className='admin-entrie' 
+              type="text" 
+              placeholder='Usuário ou e-mail' 
+              onChange={e => setUserState({ ...userState, user: e.target.value })}
+            />
             <label htmlFor="">Senha</label>
-            <input className='admin-entrie' type="password" placeholder='Senha'/>
-            <input className='admin-btn' type="submit" value='Acessar'/>
+            <input 
+              className='admin-entrie' 
+              type="password" 
+              placeholder='Senha'
+              onChange={e => setUserState({ ...userState, password: e.target.value })}
+            />
+            <input className='admin-btn' type="submit" value='Acessar' onClick={() => login()} />
         </div>
     </div>
   )
