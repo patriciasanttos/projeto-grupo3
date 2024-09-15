8e3c45e7f605921585b54a1565db7104043a15e8
unknown <hector.02220x@gmail.com>
2024-09-14 21:49:15 -0300

    Feat: API implemented to Landing Page

--- app/src/components/cardAnimal/CardAnimal.jsx
+++ app/src/components/cardAnimal/CardAnimal.jsx
@@ -1,13 +1,13 @@
 import styles from './styles.module.css';
 
-const CardAnimal = ({ image, name, gender, breed, age, onClickButton }) => {
+const CardAnimal = ({ image, name, gender, race, age, onClickButton }) => {
   return (
     <div className={styles.card}>
       <img src={image} alt={`Foto de ${name}`} className={styles.cardImage} />
       <div className={styles.cardContent}>
         <h3>{name}</h3>
         <p>{gender}</p>
-        <p>{breed}</p>
+        <p>{race}</p>
         <p>{age}</p>
         <button className={styles.cardButton} onClick={onClickButton}>
           Conheça minha história
--- app/src/services/api/index.js
+++ app/src/services/api/index.js
@@ -1,30 +1,21 @@
 import axiosClient from '../axiosClient';
 
-const axiosGet = async (url) => {
+export const axiosGet = async (url) => {
     const res = await axiosClient.get(url);
     return res.data;
 };
 
-const axiosPost = async (url, data) => {
+export const axiosPost = async (url, data) => {
     const res = await axiosClient.post(url, data);
     return res.data;
 };
 
-const axiosPut = async (url, data) => {
+export const axiosPut = async (url, data) => {
     const res = await axiosClient.put(url, data);
     return res.data;
 };
 
-const axiosDelete = async (url) => {
+export const axiosDelete = async (url) => {
     const res = await axiosClient.delete(url);
     return res.data;
 };
-
-const axiosMethods = {
-    axiosGet,
-    axiosPost,
-    axiosPut,
-    axiosDelete,
-}
- 
-export default axiosMethods;
