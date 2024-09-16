29bd520c4fe7561d3c37ac04f828b7e6b4f226a2
Patricia Santos <patriciasanttos.dev@gmail.com>
2024-09-16 16:24:22 -0300

    added navbar admin icon

---
+++ app/src/pages/private/admin_page/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import AdminNavBar from "../../../components/admin_navbar/AdminNavBar";
+
+function AdminPage() {
+  return <AdminNavBar headerTitle="Administrador">
+
+  </AdminNavBar>;
+}
+
+export default AdminPage;
--- app/src/routes.js
+++ app/src/routes.js
@@ -1,40 +1,47 @@
-import { Routes, Route, BrowserRouter, Navigate, useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
+import {
+  Routes,
+  Route,
+  BrowserRouter,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
+import { useEffect } from "react";
 
-import { publicPages, privatePages } from './pages/pages.js';
+import { publicPages, privatePages } from "./pages/pages.js";
 
 const ScrollToTop = () => {
-    const { pathname } = useLocation();
-  
-    useEffect(() => {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-    }, [pathname]);
-  
-    return;
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+
+  return;
 };
 
 export default function MainRoutes() {
-    return (
-        <BrowserRouter>
-            <ScrollToTop />
-            <Routes>
-                <Route path="/" element={<publicPages.LandingPage />} />
-                <Route path="aboutus" element={<publicPages.AboutUs />} />
-                <Route path="adoption" element={<publicPages.Adoption />} />
-                <Route path="sponsorship" element={<publicPages.Sponsorship />} />
-                <Route path="contact" element={<publicPages.Contact />} />
-                <Route path="donation" element={<publicPages.Donation />} />
-                <Route path="volunteers" element={<publicPages.Volunteers />} />
-                <Route path='admin'>
-                    <Route index element={<Navigate to='/admin/login' />} />
-                    <Route path='login' element={<privatePages.Login />} />
-                    <Route path='control_panel' element={<privatePages.ControlPanel />} />
-                    <Route path='animals' element={<privatePages.Animals />} />
-                    <Route path='sponsorships' element={<privatePages.Sponsorships />} />
-                    <Route path='adoptions' element={<privatePages.Adoptions />} />
-                    <Route path='volunteers' element={<privatePages.Volunteers />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<publicPages.LandingPage />} />
+        <Route path="aboutus" element={<publicPages.AboutUs />} />
+        <Route path="adoption" element={<publicPages.Adoption />} />
+        <Route path="sponsorship" element={<publicPages.Sponsorship />} />
+        <Route path="contact" element={<publicPages.Contact />} />
+        <Route path="donation" element={<publicPages.Donation />} />
+        <Route path="volunteers" element={<publicPages.Volunteers />} />
+        <Route path="admin">
+          <Route index element={<Navigate to="/admin/login" />} />
+          <Route path="login" element={<privatePages.Login />} />
+          <Route path="control_panel" element={<privatePages.ControlPanel />} />
+          <Route path="animals" element={<privatePages.Animals />} />
+          <Route path="sponsorships" element={<privatePages.Sponsorships />} />
+          <Route path="adoptions" element={<privatePages.Adoptions />} />
+          <Route path="volunteers" element={<privatePages.Volunteers />} />
+          <Route path="admin_page" element={<privatePages.Admin />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
