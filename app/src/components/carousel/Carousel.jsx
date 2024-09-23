ab3ddfcfedd41bc8997d1c7bd3bf202e9cb7f2a6
Patricia Santos <patriciasanttos.dev@gmail.com>
2024-09-23 14:00:35 -0300

    update hero, animals carousel responsiveness and pagination

--- app/src/components/carousel/Carousel.jsx
+++ app/src/components/carousel/Carousel.jsx
@@ -1,20 +1,30 @@
+import "./Carousel.scss";
+import { useEffect, useState } from "react";
+import { useSwipeable } from 'react-swipeable'
+
 import CardAnimal from "../../components/cardAnimal/CardAnimal";
 import arrowLeft from "../../assets/icons/arrow-left.svg";
 import arrowRight from "../../assets/icons/arrow-right.svg";
-import "./Carousel.scss";
-import { useState } from "react";
+
+import useResponsive from "../../hooks/useResponsive";
 
 const Carousel = ({ animals, onClickCardAnimal }) => {
+  const { isMobile, isTablet } = useResponsive();
   const [initialIndex, setInitialIndex] = useState(0);
   const [endIndex, setEndIndex] = useState(2);
   const isLeftArrowDisabled = initialIndex === 0;
   const isRightArrowDisabled = endIndex === animals.length - 1;
 
-  const showAnimals = [
-    animals[initialIndex],
-    animals[endIndex - 1],
-    animals[endIndex],
-  ];
+  const getCardQuantity = () => {
+    if (isMobile)
+      return [animals[initialIndex]]
+
+    if (isTablet)
+      return [animals[initialIndex], animals[endIndex - 1]];
+
+    return [animals[initialIndex], animals[endIndex - 1], animals[endIndex]];
+  }
+  const showAnimals = getCardQuantity()
 
   const onClickLeft = () => {
     if (initialIndex > 0) {
@@ -30,8 +40,13 @@ const Carousel = ({ animals, onClickCardAnimal }) => {
     }
   };
 
+  const handlers = useSwipeable({
+    onSwipedLeft: (eventData) => onClickRight(),
+    onSwipedRight: (eventData) => onClickLeft(),
+  });
+
   return (
-    <div className="carousel">
+    <div className="carousel" {...handlers}>
       {/* Botao left */}
       <img
         style={{ visibility: isLeftArrowDisabled ? "hidden" : "visible" }}
@@ -40,15 +55,25 @@ const Carousel = ({ animals, onClickCardAnimal }) => {
         src={arrowLeft}
         alt=""
       />
+      
       {/* div container cards */}
       <div className="container-cards">
         {showAnimals.map((animal) => (
           <CardAnimal
             key={animal.id}
             image={animal.image}
-            name={animal.name.charAt(0).toUpperCase() + animal.name.slice(1).toLowerCase()}
-            gender={['m', 'M'].includes(animal.gender)  ? 'Macho': 'Fêmea'}
-            race={animal.race === 'srd' ? 'Sem raça definída' : `${animal.race.charAt(0).toUpperCase()}${animal.race.slice(1).toLowerCase()}`}
+            name={
+              animal.name.charAt(0).toUpperCase() +
+              animal.name.slice(1).toLowerCase()
+            }
+            gender={["m", "M"].includes(animal.gender) ? "Macho" : "Fêmea"}
+            race={
+              animal.race === "srd"
+                ? "Sem raça definída"
+                : `${animal.race.charAt(0).toUpperCase()}${animal.race
+                    .slice(1)
+                    .toLowerCase()}`
+            }
             age={animal.age}
             onClickButton={() => onClickCardAnimal(animal)}
           />
