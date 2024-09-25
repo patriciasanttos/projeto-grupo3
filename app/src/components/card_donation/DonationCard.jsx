8563940834e140f5d2f88b24da15d38130f0508d
unknown <hector.02220x@gmail.com>
2024-09-24 21:00:58 -0300

    Feat: Donation card image overflow fixed

--- app/src/components/card_donation/DonationCard.jsx
+++ app/src/components/card_donation/DonationCard.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
-import './DonationCard.scss';
-
 import broom from '../../assets/icons/donation/broom.svg'
 import med_kit from "../../assets/icons/donation/med_kit.svg";
 import medicine from "../../assets/icons/donation/medicine.svg";
 
+import './DonationCard.scss';
 
 function DonationCard() {
   return (
@@ -16,30 +15,38 @@ function DonationCard() {
             </div>
             <div> 
                 <h2>Kit de Limpeza</h2>
+
                 <h3> R$ 25 </h3>
                 <p>Desinfetante a base de amônia quaternária para desinfecção das baias da internação.</p>
+
                 <button>Doe Agora</button> 
             </div>
         </div>
+
         <div className="card">
-        <div class="icon-container">
-            <img src={med_kit} alt="kit medico" className="icon_donation" />
-       </div>
+            <div class="icon-container">
+                <img src={med_kit} alt="kit medico" className="icon_donation" />
+            </div>
             <div> 
                 <h2>Kit Preventivo <br />de Doenças</h2>
+
                 <h3> R$ 40 </h3>
                 <p>Vacina para imunizar cães e gatos.</p>
+
                 <button>Doe Agora</button> 
             </div>
         </div>
+
         <div className="card">
-        <div class="icon-container">
-            <img src={medicine} alt="seringa" className="icon_donation" />
-         </div>
+            <div class="icon-container">
+                <img src={medicine} alt="seringa" className="icon_donation" />
+            </div>
             <div> 
                 <h2>Kit Diária <br />de Internação</h2>
+
                 <h3> R$ 100 </h3>
                 <p>Soro, cateter para acesso, equipo esparadrapo e medicação.</p>
+
                 <button>Doe Agora</button> 
             </div>
         </div>
