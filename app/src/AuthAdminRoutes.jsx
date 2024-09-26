769555bdca0a8c3422806e9be67fd2634249bc20
Patricia Santos <patriciasanttos.dev@gmail.com>
2024-09-25 22:53:56 -0300

    update Auth Admin Routes

--- app/src/AuthAdminRoutes.jsx
+++ app/src/AuthAdminRoutes.jsx
@@ -18,12 +18,10 @@ const AuthAdminRoutes = () => {
       .then(() => {
         if (location.pathname === '/admin/login' && loginCookie)
           return navigate('/admin/control_panel');
-        
       })
       .catch(({ response }) => {
         if (response.status === 404)
           return navigate('/admin/login');
-        
       });
   }, []);
 
