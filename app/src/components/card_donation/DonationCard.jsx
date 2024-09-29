267da6255599c7beed2d09592d37f896f6b3b316
Patricia Santos <patriciasanttos.dev@gmail.com>
2024-09-29 19:44:39 -0300

    added payment link

--- app/src/components/card_donation/DonationCard.jsx
+++ app/src/components/card_donation/DonationCard.jsx
@@ -9,48 +9,79 @@ import './DonationCard.scss';
 function DonationCard() {
   return (
     <section className="donation_card">
-        <div className="card">
-            <div className="icon-container">
-                <img src={broom} alt="Icone de Limpeza" className="icon_donation"/>
-            </div>
-            <div> 
-                <h2>Kit de<br/> Limpeza</h2><br/>
-                <h3> R$ 25 </h3>
-                <br/>
-                <p>Desinfetante à base de amônia quaternária para desinfecção das baias. Mantenha o ambiente limpo e seguro!</p>
-                <br/>
-                <button>Doe Agora</button> 
-            </div>
+      <div className="card">
+        <div className="icon-container">
+          <img src={broom} alt="Icone de Limpeza" className="icon_donation" />
         </div>
+        <div>
+          <h2>
+            Kit de
+            <br /> Limpeza
+          </h2>
+          <br />
+          <h3> R$ 25 </h3>
+          <br />
+          <p>
+            Desinfetante à base de amônia quaternária para desinfecção das
+            baias. Mantenha o ambiente limpo e seguro!
+          </p>
+          <br />
+          <a
+            href="https://link.picpay.com/p/172764103166f9b5c71edaa"
+            target="blank"
+          >
+            <button>Doe Agora</button>
+          </a>
+        </div>
+      </div>
 
-        <div className="card">
-            <div className="icon-container">
-                <img src={med_kit} alt="kit medico" className="icon_donation" />
-            </div>
-            <div> 
-                <h2>Kit Preventivo <br />de Doenças</h2>
-                <br/>
-                <h3> R$ 40 </h3>
-                <br/>
-                <p>Vacina essencial para imunizar e proteger cães e gatos, garantindo a saúde e bem-estar dos nossos animais.</p>
-                <br/>
-                <button>Doe Agora</button> 
-            </div>
+      <div className="card">
+        <div className="icon-container">
+          <img src={med_kit} alt="kit medico" className="icon_donation" />
         </div>
+        <div>
+          <h2>
+            Kit Preventivo <br />
+            de Doenças
+          </h2>
+          <br />
+          <h3> R$ 50 </h3>
+          <br />
+          <p>
+            Vacina essencial para imunizar e proteger cães e gatos, garantindo a
+            saúde e bem-estar dos nossos animais.
+          </p>
+          <br />
+          <a
+            href="https://link.picpay.com/p/172764108366f9b5fb2073f"
+            target="blank"
+          >
+            <button>Doe Agora</button>
+          </a>
+        </div>
+      </div>
 
-        <div className="card">
-            <div className="icon-container">
-                <img src={medicine} alt="seringa" className="icon_donation" />
-            </div>
-            <div> 
-                <h2>Kit Diária <br />de Internação</h2> 
-                <br/>
-                <h3> R$ 100 </h3> <br/>
-                <p>Soro, cateter, equipo, esparadrapo e medicação. Essencial para o cuidado e a recuperação dos animais.</p>
-                <br/>
-                <button>Doe Agora</button> 
-            </div>
+      <div className="card">
+        <div className="icon-container">
+          <img src={medicine} alt="seringa" className="icon_donation" />
         </div>
+        <div>
+          <h2>
+            Kit Diária <br />
+            de Internação
+          </h2>
+          <br />
+          <h3> R$ 100 </h3> <br />
+          <p>
+            Soro, cateter, equipo, esparadrapo e medicação. Essencial para o
+            cuidado e a recuperação dos animais.
+          </p>
+          <br />
+          <a href="https://link.picpay.com/p/172764116966f9b651b136f" target='blank'>
+            <button>Doe Agora</button>
+          </a>
+        </div>
+      </div>
     </section>
   );
 }
